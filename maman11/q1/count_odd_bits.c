ca632aa2a9349dacbd082c0ce731e58541a09183
nmagnezi <nmagnezi@redhat.com>
2015-04-10 02:50:08 +0300

    maman11_q1

--- maman11/q1/count_odd_bits.c
+++ maman11/q1/count_odd_bits.c
@@ -13,23 +13,23 @@ Input validation was not required in the task. therefore I presume the user will
 /*This function returns the number of bits equal to 1 in odd indexes*/
 int count_odd_bits(unsigned long decimal_input)
 {
- int count=0;
- decimal_input>>=1; /*Skip the 0 index bit*/
- while(decimal_input) /*While there are still bits to check*/
- {
-  if((decimal_input & 1) == 1) /*isolate the rightmost bit and check if it's equal to 1*/
-  {
-   count++;
-  }
- decimal_input>>=2; /*get to the next odd index bit*/
-}
-return count;
-}
+	int count=0;
+	decimal_input>>=1; /*Skip the 0 index bit*/
+	while(decimal_input) /*While there are still bits to check*/
+	{
+		if((decimal_input & 1) == 1) /*isolate the rightmost bit and check if it's equal to 1*/
+		{
+			count++;
+		}
+		decimal_input>>=2; /*get to the next odd index bit*/
+	}
+	return count;
+	}
 
 int main()
 {
- unsigned long decimal_input;
- scanf("%lu", &decimal_input);
- printf("%d\n", count_odd_bits(decimal_input));
- return 0;
+	unsigned long decimal_input;
+	scanf("%lu", &decimal_input);
+	printf("%d\n", count_odd_bits(decimal_input));
+	return 0;
 }
