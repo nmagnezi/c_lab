e0f8a8376214970f2259c521b781d69fc982cd8b
nmagnezi <nmagnezi@redhat.com>
2015-04-10 03:09:43 +0300

    maman11_q2 docstrings

--- maman11/q2/letters.c
+++ maman11/q2/letters.c
@@ -1,3 +1,14 @@
+/*
+maman11_q2 - letters.c
+* Description:
+The Program gets chars from standard input and prints them with the following modifications:
+1. At the beginning of each sentence, if the first char is a letter, it is printed in upper case.
+2. Quoted expressions printed in upper case.
+3. Rest of the chars printed in lower case.
+4. Digits are Omitted.
+5. Blank line should be printed between each 2 lines.
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 
@@ -15,28 +26,28 @@ int main()
 
 	while ((input_char = getchar()) != EOF)
 	{
-		if isdigit(input_char) continue;
+		if isdigit(input_char) continue; /* no processing needed for digits */
 
 		switch(input_char)
 		{
-		case WHITESPACE:
+		case WHITESPACE: /* avoid changing upper case or lower case flags for whitespace char */
 			break;
 
-		case END_OF_SENTENCE:
+		case END_OF_SENTENCE: /* next letter should be upper case */
 			first_char_in_sentence = 1;
 			break;
 
-		case NEW_LINE_CHAR:
+		case NEW_LINE_CHAR: /* next letter should be upper case */
 			first_char_in_sentence = 1;
 			break;
 
-		case QUOTE_CHAR:
-			if (first_quote_char)
+		case QUOTE_CHAR: /* handles quoted expression */
+			if (first_quote_char) /* opens quoted expression */
 			{
 				print_in_quote = 1;
 				first_quote_char = 0;
 			}
-			else
+			else /* ends quoted expression */
 			{
 				print_in_quote = 0;
 				first_quote_char = 1;
@@ -44,23 +55,23 @@ int main()
 		}
 
 		if ((input_char == END_OF_SENTENCE) || (input_char == WHITESPACE) || (input_char == QUOTE_CHAR))
-		{
+		{   /* handles chars that should be printed without changing flags */
 			printf("%c", input_char);
 		}
 		else if (input_char == NEW_LINE_CHAR)
-		{
+		{   /* handles the double newline print */
 			printf("\n\n");
 		}
 		else if ((print_in_quote) || (first_char_in_sentence))
-		{
+		{	/* handles all cases of upper case prints*/
 			printf("%c",  toupper(input_char));
 			if (first_char_in_sentence)
-			{
+			{	/* if upper case print is due to a first char in sentence, Disable it for next char */
 				first_char_in_sentence = 0;
 			}
 		}
 		else
-		{
+		{   /* other cases are lower case prints */
 			printf("%c", tolower(input_char));
 		}
 
