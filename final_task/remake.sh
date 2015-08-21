29d8a94833dfbc46d3545180a4147c18d7268f4c
Nir Magnezi <nmagnezi@redhat.com>
2015-08-22 00:15:45 +0300

    renaming folders

---
+++ final_task/remake.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# helper script to remake the whole program.
+rm assembler
+find . -name "*.ext" -type f -delete
+find . -name "*.ent" -type f -delete
+find . -name "*.obj" -type f -delete
+find . -name "*.o" -type f -delete
+#make
